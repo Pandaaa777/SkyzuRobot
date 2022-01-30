5f3ea8731fe4e9ad3e7ba4eed2bc77681fccb0d1
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-01-30 15:15:47 +0700

    ..

--- SkyzuRobot/modules/helper_funcs/chat_status.py
+++ SkyzuRobot/modules/helper_funcs/chat_status.py
@@ -319,11 +319,11 @@ def can_promote(func):
         message_chat_title = update.effective_message.chat.title
 
         if update_chat_title == message_chat_title:
-            cant_promote = "I can't promote/demote people here!\nMake sure I'm admin and can appoint new admins."
+            cant_promote = "I can't promote/demote people here!\nMake sure I'm admin and can appoint new admins"
         else:
             cant_promote = (
                 f"I can't promote/demote people in <b>{update_chat_title}</b>!\n"
-                f"Make sure I'm admin there and can appoint new admins."
+                f"Make sure I'm admin there and can appoint new admins"
             )
 
         if chat.get_member(bot.id).can_promote_members:
